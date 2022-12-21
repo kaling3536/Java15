2323d8d0904a8327239b8be4a1b31237d6fc6ce1
kaling3536 <107583599+kaling3536@users.noreply.github.com>
2022-12-21 14:43:30 +0800

    Update Class02.java

--- Class02.java
+++ Class02.java
@@ -1,3 +1,4 @@
+#abc
 class CTest extends Thread
 
 {
@@ -24,3 +25,10 @@ public class hw15_2 {
 		bye.run();
 	}
 }
+
+#d
+程式執行時,會先將"Hello 1"~"Hello 5"列印完畢之後,再印出"Good bye 1"~"Good
+bye 5"。要使用多執行緒時,必須要用 start() method 來啟動,由 start() method 在
+排程器中登錄該執行緒,當這個執行緒開始執行,run() method 自然就會被呼叫。
+像 hw15_2 直接呼叫 run() method,只是在程式中將 run() method 執行,並不會啟
+動多執行緒。
